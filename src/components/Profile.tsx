5dd5fdce7f755f143dcb4cf4f422dab761637f04
Juliano Vendramini <JulianoVendramini@outlook.com>
2021-02-23 13:34:17 -0300

    add components & styles. Creating a profile and a countdown

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile(){
+    return(
+        <div className = {styles.profileContainer}>
+            <img src="https://github.com/JulianoVendramini.png" alt="Juliano Vendramini"/>
+            <div>
+                <strong>Juliano Vendramini</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 3
+                </p>
+            </div>
+        </div>
+    );
+}
